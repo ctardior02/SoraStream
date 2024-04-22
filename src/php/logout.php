64d49153140d29e9055d29639097ff00280077e4
Carlos Tardio Rodriguez <ctardior@gmail.com>
2024-04-22 13:46:06 +0200

    Añadimos el cerrar sesion

---
+++ src/php/logout.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();//el script se una a la sesion
+
+    unset($_SESSION['precios']);
+
+    /* Para cerrar la sesion es necesario borrar todas las variables de la sesion, para ello inicilizamos el array $_sesion */
+    $_SESSION = array();
+
+    /* Ademas se debe utilizar la funcion sesion_destroy() */
+    session_destroy();
+
+    /* finalmete el script lleva de nuevo al login: */
+    header("Location: ./register.php");
+
+?>
