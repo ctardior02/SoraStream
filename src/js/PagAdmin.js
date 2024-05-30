1cef56740830963df8d4727ed6f1fd224676d614
Aorasio <adrianprasio@gmail.com>
2024-05-30 11:57:50 +0200

    Pagina administradores actualizada

--- src/js/PagAdmin.js
+++ src/js/PagAdmin.js
@@ -13,3 +13,39 @@ function mostrarPestaña(tabId) {
     // Activar la pestaña seleccionada
     event.target.classList.add('activa');
 }
+
+/* Estilos dropdown index cuenta */
+function myFunction() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+    
+    // Ajustar la posición del dropdown si se sale de la pantalla
+    var rect = dropdown.getBoundingClientRect();
+    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    
+    if (rect.right > viewportWidth) {
+      dropdown.style.right = "0";
+      dropdown.style.left = "auto";
+    } else {
+      dropdown.style.right = "auto";
+      dropdown.style.left = "0";
+    }
+  }
+  
+  // Cierra el dropdown si se hace clic fuera de él
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  function reproducir(id){
+    location.href ="./src/php/EleccionCapitulo.php?id=" + id;
+  }
+  /* Fin estilos dropdwon cuenta */
--- src/php/PagAdmin.php
+++ src/php/PagAdmin.php
@@ -1,79 +1,156 @@
+<?php
+session_start();
+if (!isset($_SESSION["rol"]) || $_SESSION["rol"] != 2) {
+  header("Location: ../../index.php");
+}
+function mostrarFoto()
+{
+  $directorio = "../img/imgs-usuarios";
+  $carpeta = scandir($directorio);
+  $jpgUsuario = $_SESSION["nick"] . ".jpg";
+  $dir = (in_array($jpgUsuario,  $carpeta)) ? $directorio . "/" . $jpgUsuario : $directorio . "/default.webp";
+  return $dir;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SoraStream-Admin</title>
-    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/cabecera.css">
-    <link rel="stylesheet" href="../css/PagAdmin.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SoraStream-Admin</title>
+  <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/cabecera.css">
+  <link rel="stylesheet" href="../css/PagAdmin.css">
 </head>
 
 <body class="d-flex flex-column justify-content-center body">
-    <header class="d-flex  flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom">
-        <div class=" ms-2 mb-2 mb-md-0 d-flex justify-content-center align-items-center">
-            <a href="../..//Index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
-                <img src="../../src/img/LogoSoraStream3.png" width="200px" alt="">
-            </a>
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <!-- <li><a href="#" class="px-2 BotonHeader">Home</a></li> -->
-                <li><a href="#" class="px-2 BotonHeader">Tu lista</a></li>
-                <li><a href="./categorias.php" class="px-2 BotonHeader">Categorias</a></li>
-            </ul>
-        </div>
-        <div class="d-flex align-items-center">
-            <?php
+  <header class="d-flex  flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom">
+    <div class=" ms-2 mb-2 mb-md-0 d-flex justify-content-center align-items-center headerIzq">
+      <a href="../../Index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+        <img src="../img/LogoSoraStream3.png" width="200px" alt="">
+      </a>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <!-- <li><a href="#" class="px-2 BotonHeader">Home</a></li> -->
+        <li><a href="#" class="px-2 BotonHeader">Tu lista</a></li>
+        <li><a href="./categorias.php" class="px-2 BotonHeader">Categorias</a></li>
+        <?php
+        if (isset($_SESSION["rol"])) {
+          if ($_SESSION["rol"] == 2) {
+            echo "<li><a href='./src/php/PagAdmin.php' class='px-2 BotonHeader'>Administradores</a></li>";
+          }
+        }
+        ?>
+
+      </ul>
+    </div>
+    <div class="d-flex align-items-center">
+      <?php
 
 
-            if (!isset($_SEñSSION["id"])) {
-                echo '  <div class="me-2 text-end d-flex">
-              <a href ="./src/php/register.php" class="BotonHeader font-sm bold auth-link">Registrarse</a>
-              <a href="./src/php/register.php" class="BotonInicioRegistro font-sm bold auth-link">Iniciar sesión</a>
+      if (!isset($_SESSION["id"])) {
+        echo '  <div class="me-2 text-end d-flex">
+              <a href ="./src/php/Login.php" class="BotonHeader font-sm bold auth-link">Registrarse</a>
+              <a href="./src/php/Login.php" class="BotonInicioRegistro font-sm bold auth-link">Iniciar sesión</a>
             </div>';
-            } else {
-                echo '<div class="dropdown">
-              <button onclick="myFunction()" class="dropbtn">Logout</button>
+      } else {
+        echo '<div class="dropdown">
+              <button onclick="myFunction()" class="dropbtn">
+                <img src="' . mostrarFoto() . '" width="200px" class="dropbtn" alt="">
+              </button>
               <div id="myDropdown" class="dropdown-content">
                 <a href="./src/php/cuenta.php">Configuración de la cuenta</a>
-                <a href="#">Cerrar sesión</a>
+                <a href="./src/php/logout.php">Cerrar sesión</a>
               </div>
             </div>
             ';
-            }
+      }
 
-            ?>
+      ?>
 
-        </div>
-    </header>
-    <div class="contenedorFormularios">
-    <div class="pestañas">
-            <div class="pestaña activa" id="animePestaña" onclick="mostrarPestaña('anime')">Crear Anime</div>
-            <div class="pestaña" onclick="mostrarPestaña('capitulo')">Crear Capítulo</div>
-        </div>
-        <div id="anime" class="content activa form-container">
-            <h2 class="text-light">Crear Anime</h2>
-            <form class="formularios">
-                <input type="text" class="izquierda" placeholder="Título" required>
-                <input type="text" placeholder="Descripción" required>
-                <input type="text" class="izquierda" placeholder="Categoría" required>
-                <button type="submit">Guardar Anime</button>
-            </form>
-        </div>
-        <div id="capitulo" class="content form-container">
-            <h2  class="text-light">Crear Capítulo</h2>
-            <form class="formularios">
-                <input type="text" class="izquierda" placeholder="Título del Episodio" required>
-                <input type="number" placeholder="Temporada" required>
-                <input type="number" class="izquierda" placeholder="Número de Episodio" required>
-                <input type="url" placeholder="URL" required>
-                <input type="number" class="izquierda" placeholder="Duración (minutos)" required>
-                <input type="number" placeholder="ID del Anime" required>
-                <button type="submit">Guardar Capítulo</button>
-            </form>
-        </div>
     </div>
-    <script src="../js/PagAdmin.js"></script>
+  </header>
+  <div class="contenedorFormularios">
+    <div class="bg-dark p-5 mr-2 d-flex align-items-center flex-column justify-content-center">
+      <div class="pestañas">
+        <div class="pestaña " id="animePestaña" onclick="mostrarPestaña('anime')">Crear Anime</div>
+        <div class="pestaña activa" onclick="mostrarPestaña('capitulo')">Crear Capítulo</div>
+      </div>
+      <div id="anime" class="content  form-container">
+        <h2 class="text-light">Crear Anime</h2>
+        <form class="formularios" method="post">
+          <input type="text" class="izquierda" name="titulo" placeholder="Título" required>
+          <input type="text" class="izquierda categoria" name="categoria" placeholder="Categoría" required>
+          <textarea name="Descripción" class="textAreaDescripcion" placeholder="Descripción" required></textarea>
+          <button type="submit" value="CrearAnime" class="mb-4" name="CrearAnime">Guardar Anime</button>
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['CrearAnime'])) {
+          include "./db.php";
+
+          // Recoger los datos del formulario
+          $titulo = htmlspecialchars($_POST["titulo"]);
+          $descripcion = htmlspecialchars($_POST["descripcion"]);
+          $categoria = htmlspecialchars($_POST["categoria"]);
+
+          // Preparar y ejecutar la consulta
+          $idAnime = $db->prepare("INSERT INTO animes (Titulo, Descripcion, Categoria) VALUES (:titulo, :descripcion, :categoria)");
+          $idAnime->bindParam(":titulo", $titulo);
+          $idAnime->bindParam(":descripcion", $descripcion);
+          $idAnime->bindParam(":categoria", $categoria);
+          $idAnime->execute();
+
+          echo "<strong class='w-100 d-flex justify-content-center textoCreacion'>Anime creado</strong>";
+        }
+        ?>
+      </div>
+
+      <div id="capitulo" class="content form-container activa">
+        <h2 class="text-light">Crear Capítulo</h2>
+        <form class="formularios formularioCapitulo" method="post">
+          <input type="number" placeholder="Temporada" name="Temporada" required>
+          <input type="number" class="izquierda" name="NumeroEpisodio" placeholder="Número de Episodio" required>
+          <input type="number" class="izquierda" name="Duracion" placeholder="Duración (minutos)" required>
+          <input type="number" name="IDAnime" placeholder="ID del Anime" required>
+          <input type="text" class="TituloEpisodio" name="TituloEpisodio" placeholder="Título del Episodio" required>
+          <button type="submit" name="CrearCapitulo" class="mb-4" value="CrearCapitulo">Guardar Capítulo</button>
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['CrearCapitulo'])) {
+          include "./db.php";
+          // Recoger los datos del formulario
+          $numeroEpisodio = htmlspecialchars($_POST["NumeroEpisodio"]);
+          $duracion = htmlspecialchars($_POST["Duracion"]);
+          $idAnime = htmlspecialchars($_POST["IDAnime"]);
+          $temporada = htmlspecialchars($_POST["Temporada"]);
+          $tituloEpisodio = htmlspecialchars($_POST["TituloEpisodio"]);
+
+          //consultar si existe el IDanime en la tabla de animes
+          $consultaAnime = $db->prepare("SELECT * FROM animes WHERE ID = :idAnime");
+          $consultaAnime->bindParam(":idAnime", $idAnime);
+          $consultaAnime->execute();
+          $resultadoAnime = $consultaAnime->fetch();
+
+          if ($resultadoAnime == null) {
+            echo "<strong class='w-100 d-flex justify-content-center text-warning'>No se ha encontrado el anime con el ID " . $idAnime . "</strong>";
+          } else {
+            $idAnimeStmt = $db->prepare("INSERT INTO capitulos (Num_Episodio, Titulo, Temporada, Duracion, ID_Anime) VALUES (:numeroEpisodio, :tituloEpisodio, :temporada, :duracion, :idAnime)");
+            $idAnimeStmt->bindParam(":numeroEpisodio", $numeroEpisodio);
+            $idAnimeStmt->bindParam(":tituloEpisodio", $tituloEpisodio);
+            $idAnimeStmt->bindParam(":temporada", $temporada);
+            $idAnimeStmt->bindParam(":duracion", $duracion);
+            $idAnimeStmt->bindParam(":idAnime", $idAnime);
+            $idAnimeStmt->execute();
+
+            echo "<strong class='w-100 d-flex justify-content-center textoCreacion'>Anime creado</strong>";
+          }
+        }
+        ?>
+      </div>
+    </div>
+  </div>
+  <script src="../js/PagAdmin.js"></script>
+  <script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
