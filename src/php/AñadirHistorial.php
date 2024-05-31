8e08f7916f5ed4bb9042412f376409ffe6542cac
Carlos Tardio Rodriguez <ctardior@gmail.com>
2024-05-31 21:23:34 +0200

    Error en la hora del historial solucionado

--- src/php/AñadirHistorial.php
+++ src/php/AñadirHistorial.php
@@ -4,6 +4,7 @@ $data = file_get_contents("php://input");
 include "./db.php";
 
 $hist = $db->prepare("INSERT INTO historial (Fecha, anime, usuario) VALUES (:fecha, :idAnime, :idUsuario)");
+date_default_timezone_set('Europe/Madrid');
 $hist->bindParam(":fecha", date("Y-m-d H:i:s"));
 $hist->bindParam(":idAnime", $data);
 $hist->bindParam(":idUsuario", $_SESSION["id"]);
