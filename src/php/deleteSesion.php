4b55e54bd15769457df4f06383dd69678d450b60
Aorasio <adrianprasio@gmail.com>
2024-06-02 21:23:33 +0200

    Delete sesion arreglado

--- src/php/deleteSesion.php
+++ src/php/deleteSesion.php
@@ -2,6 +2,14 @@
     session_start();
     include "./db.php";
         try {
+            $eliminarFav = $db->prepare("DELETE FROM favoritos WHERE ID_Usuario = :id");
+            $eliminarFav->bindParam(':id', $_SESSION['id']);
+            $eliminarFav->execute();
+            
+            $eliminarHist = $db->prepare("DELETE FROM historial WHERE usuario = :id");
+            $eliminarHist->bindParam(':id', $_SESSION['id']);
+            $eliminarHist->execute();
+
             $preparada = $db->prepare("DELETE FROM usuarios WHERE ID = :id");
             $preparada->bindParam(':id', $_SESSION['id']);
             $preparada->execute();
